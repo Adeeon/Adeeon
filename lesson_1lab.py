7433165ab4a3aa07fc33294069abcb2fde283dd8
Adeeon <cgt10dt@gmail.com>
2023-01-11 18:39:59 -0600

    a

--- lesson_1lab.py
+++
@@ -1,4 +0,0 @@
-print('Python is fun!') # runs the data as a statement
-print('p' + 'y' + 't' + 'h' + 'o' + 'n') # runs data combining the data to make a single word
-print('Python' + ' is' + ' fun!') # runs the combination of data to make a sentence, with the space included
-print('P' + 'y' + 't' + 'h' + 'o' + 'n' +' ' + 'i' + 's' + ' ' 'f' 'u' 'n' '!')  # runs the combination of data with spaces inputed as ' '
